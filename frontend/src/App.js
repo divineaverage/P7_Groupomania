8c33bede4126c8388006b8e8597a0a5bf9fe0b4a
Lindsay J <lindsay.jawor@gmail.com>
2023-05-02 21:28:48 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../src/sass/app.scss"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./components/Auth"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App 
