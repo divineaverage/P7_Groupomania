fc79b915373437c45829457938520cedb07a7e1e
Lindsay J <lindsay.jawor@gmail.com>
2023-05-17 14:16:28 -0400

    fixed store

---
+++ client/src/components/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+
+import user from './userSlice'
+
+const reducer = combineReducers({
+  user,
+})
+
+const store = configureStore({
+  reducer,
+})
+
+export default store;
